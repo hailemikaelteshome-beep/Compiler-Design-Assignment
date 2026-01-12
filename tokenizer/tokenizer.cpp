773807d7ee4880a16719f1daf590d93245890733
hailemikaelteshome-beep <hailemikaelteshome@gmail.com>
2026-01-12 04:17:02 -0800

    Update tokenizer.cpp

--- tokenizer/tokenizer.cpp
+++ tokenizer/tokenizer.cpp
@@ -1,1 +1,19 @@
+#ifndef TOKENIZER_H
+#define TOKENIZER_H
+
+#include <string>
+#include <vector>
+
+enum class TokenType {
+    NUMBER, PLUS, MINUS, MULTIPLY
+};
+
+struct Token {
+    TokenType type;
+    std::string value;
+};
+
+std::vector<Token> tokenize(const std::string& input);
+
+#endif
 
